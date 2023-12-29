89f36f5df48f6b58c1e6494a88cd60a411c8f2a2
Alireza Moghaddam <armoghaddam24@gmail.com>
2023-12-29 11:14:22 +0330

    feat(forms): add auto-focus verification fields
    Add a 4-number verification component with auto-advance focus. Includes a CLEAR button to reset fields.
    
    - Fields accept and auto-advance on input
    - Initial focus set on the first field
    - CLEAR button resets all inputs
    
    Closes #3

--- src/Pages/Verification.tsx
+++ src/Pages/Verification.tsx
@@ -1,10 +1,38 @@
-import {Text} from "@chakra-ui/react";
+import {Button, Flex, PinInput, PinInputField, Text,} from "@chakra-ui/react";
+import {useState} from "react";
+import {MdClose} from "react-icons/md";
 
 const Verification = () => {
+
+    const [pin, setPin] = useState("")
+
+    const onChange = (pin: string) => {
+        setPin(pin)
+    }
+    const clear = () => {
+        setPin("")
+    }
+
     return (
-        <div>
-            <Text>Verification</Text>
-        </div>
+        <Flex direction={"column"} justify={"center"} alignItems={"start"} gap={4}>
+            <Text fontWeight={"bold"}>Enter your four digits verification code</Text>
+            <Flex w={"100%"} justify={"center"} gap={4}>
+                <PinInput autoFocus value={pin} onChange={onChange}>
+                    <PinInputField/>
+                    <PinInputField/>
+                    <PinInputField/>
+                    <PinInputField/>
+                </PinInput>
+            </Flex>
+            <Flex w={"100%"} justify={"end"} gap={4}>
+                <Button colorScheme="blue" variant="outline">
+                    Verify
+                </Button>
+                <Button colorScheme="red" variant="outline" onClick={() => clear()} leftIcon={<MdClose/>}>
+                    clear
+                </Button>
+            </Flex>
+        </Flex>
     );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
 import {AppRouter} from "./Router.tsx";
+import {ChakraProvider} from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={AppRouter}/>
+        <ChakraProvider>
+            <RouterProvider router={AppRouter}/>
+        </ChakraProvider>
     </React.StrictMode>,
 )
